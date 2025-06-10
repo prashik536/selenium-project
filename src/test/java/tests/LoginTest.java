da8657e1546276157e955e6aacc1977601f84f3d
Prashik Kamble <138839248+prashik536@users.noreply.github.com>
2025-06-11 01:58:06 +0530

    Update LoginTest.java

--- src/test/java/tests/LoginTest.java
+++ src/test/java/tests/LoginTest.java
@@ -1,15 +1,15 @@
 package tests;
 
-import org.junit.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class LoginTest {
+
     @Test
-    public void testLogin() {
-        System.setProperty("webdriver.chrome.driver", "/usr/local/bin/chromedriver");
-        WebDriver driver = new ChromeDriver();
-        driver.get("https://example.com");
-        driver.quit();
+    public void sampleTest() {
+        int expected = 5;
+        int actual = 2 + 3;
+        assertEquals(expected, actual, "Expected result does not match actual result.");
     }
 }
